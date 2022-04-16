de8ef4f0db6244c498448fca2b0cf3ea77bb899f
ramya45r <103188616+ramya45r@users.noreply.github.com>
2022-04-16 17:09:40 +0530

    Add files via upload

--- apple.js
+++ apple.js
@@ -3,7 +3,7 @@ function validate(event){
     var apple=document.getElementById("apple").value;
     var password=document.getElementById("pass").value;
     if(apple=="123456"&&password=="RAMYA"){
-        location.replace("https://ramya45r.github.io/apple-responsive-/index.html")
+        location.replace("https://ramya45r.github.io/Apple-responsive-/")
         return;
     }else{
     alert("enter valid number and password");
